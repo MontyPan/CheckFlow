3a4b0c5b344ad9d268b0f5f3b1fe87f94295935c
MontyPan <monty.pan@dontcareabout.us>
2017-12-22 15:23:15 +0800

    增加：設定檔 VO 與預設設定檔

---
+++ src/main/java/us/dontcareabout/CheckFlow/server/Setting.java
@@ -0,0 +1,21 @@
+package us.dontcareabout.CheckFlow.server;
+
+import java.io.File;
+
+import us.dontcareabout.java.DoubleProperties;
+
+public class Setting extends DoubleProperties {
+	private static File workspace;
+
+	public Setting() {
+		super("dev-setting.xml", "CheckFlow.xml");
+	}
+
+	public File workspace() {
+		if (workspace == null) {
+			workspace = new File(getProperty("workspace"));
+		}
+
+		return workspace;
+	}
+}
