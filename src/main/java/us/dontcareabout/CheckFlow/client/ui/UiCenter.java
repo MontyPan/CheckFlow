bb3f134b959dc5707aaa838de873b04ba1901666
MontyPan <monty.pan@dontcareabout.us>
2017-12-23 10:57:30 +0800

    UiCenter 基礎建設

---
+++ src/main/java/us/dontcareabout/CheckFlow/client/ui/UiCenter.java
@@ -0,0 +1,31 @@
+package us.dontcareabout.CheckFlow.client.ui;
+
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.Widget;
+import com.sencha.gxt.widget.core.client.container.Viewport;
+
+import us.dontcareabout.CheckFlow.client.view.CheckFlowView;
+
+public class UiCenter {
+	private final static Viewport viewport = new Viewport();
+	private static CheckFlowView checkFlowView;
+
+	public static void start() {
+		RootPanel.get().add(viewport);
+		checkFlowList();
+	}
+
+	public static void checkFlowList() {
+		if (checkFlowView == null) {
+			checkFlowView = new CheckFlowView();
+		}
+
+		switchTo(checkFlowView);
+	}
+
+	private static void switchTo(Widget widget) {
+		viewport.clear();
+		viewport.add(widget);
+		viewport.forceLayout();
+	}
+}
