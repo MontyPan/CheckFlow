60769d2f907aacdfde5a9a392dc55a9c937fc173
MontyPan <monty.pan@dontcareabout.us>
2018-01-05 18:08:12 +0800

    增加：下方 tool bar 區

--- src/main/java/us/dontcareabout/CheckFlow/client/view/CheckFlowView.java
+++ src/main/java/us/dontcareabout/CheckFlow/client/view/CheckFlowView.java
@@ -2,10 +2,12 @@ package us.dontcareabout.CheckFlow.client.view;
 
 import java.util.ArrayList;
 
+import com.sencha.gxt.chart.client.draw.RGB;
 import com.sencha.gxt.core.client.dom.ScrollSupport.ScrollMode;
 import com.sencha.gxt.widget.core.client.container.VerticalLayoutContainer;
 
 import us.dontcareabout.CheckFlow.client.component.CheckFlowInfo;
+import us.dontcareabout.CheckFlow.client.component.TextButton;
 import us.dontcareabout.CheckFlow.client.data.CheckFlowReadyEvent;
 import us.dontcareabout.CheckFlow.client.data.CheckFlowReadyEvent.CheckFlowReadyHandler;
 import us.dontcareabout.CheckFlow.client.data.DataCenter;
@@ -22,6 +24,7 @@ public class CheckFlowView extends VerticalLayoutContainer {
 		main.add(list, new VerticalLayoutData(1, -1));
 		main.setScrollMode(ScrollMode.AUTOY);
 		add(main, new VerticalLayoutData(1, 1));
+		add(new Toolbar(), new VerticalLayoutData(1, 80));
 
 		DataCenter.addCheckFlowReady(new CheckFlowReadyHandler() {
 			@Override
@@ -66,6 +69,48 @@ public class CheckFlowView extends VerticalLayoutContainer {
 			}
 
 			super.onResize(width, height);
+		}
+	}
+
+	class ToolItem extends TextButton {
+		public ToolItem(String str) {
+			super(str);
+			setBgColor(RGB.LIGHTGRAY);
+			setBgRadius(20);
+			setMargin(20);
+		}
+	}
+
+	class Toolbar extends LayerContainer {
+		ToolItem add = new ToolItem("新增專案");
+		ToolItem checkTemplate = new ToolItem("流程範本管理");
+
+		ArrayList<LayerSprite> items = new ArrayList<>();
+
+		public Toolbar() {
+			add(add);
+			add(checkTemplate);
+		}
+
+		@Override
+		protected void onResize(int width, int height) {
+			//Refactory 抽去 GF：HorizontalLayer
+			final int margin = 10;
+			double wUnit = width / items.size() - margin * 2;
+			int index = 0;
+
+			for (LayerSprite btn : items) {
+				btn.onResize(wUnit, height);
+				btn.setLX(margin + index * (wUnit + margin * 2));
+				index++;
+			}
+
+			super.onResize(width, height);
+		}
+
+		private void add(LayerSprite item) {
+			addLayer(item);
+			items.add(item);
 		}
 	}
 }
