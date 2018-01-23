6e77800a608e2dccea7d6dfddbb6ed91f4c8271d
MontyPan <monty.pan@dontcareabout.us>
2018-01-23 16:40:18 +0800

    修正：指定讀取 JSON 時的 charset

--- src/main/java/us/dontcareabout/CheckFlow/server/RpcServiceImpl.java
+++ src/main/java/us/dontcareabout/CheckFlow/server/RpcServiceImpl.java
@@ -1,8 +1,9 @@
 package us.dontcareabout.CheckFlow.server;
 
 import java.io.File;
-import java.io.FileReader;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.ArrayList;
@@ -64,7 +65,7 @@ public class RpcServiceImpl extends RemoteServiceServlet implements RpcService {
 
 	private CheckFlow load(File file) {
 		try {
-			FileReader fr = new FileReader(file);
+			InputStreamReader fr = new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8);
 			CheckFlow result = gson.fromJson(fr, CheckFlow.class);
 			fr.close();
 			return result;
