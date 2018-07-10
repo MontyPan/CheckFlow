860b2b3ddbed67d7985f9a8a53dab4c78115fabc
MontyPan <monty.pan@dontcareabout.us>
2018-07-10 16:48:30 +0800

    refactor：抽出 Block

---
+++ src/main/java/us/dontcareabout/CheckFlow/client/Formate.java
@@ -0,0 +1,7 @@
+package us.dontcareabout.CheckFlow.client;
+
+import com.google.gwt.i18n.client.NumberFormat;
+
+public class Formate {
+	public static final NumberFormat NUMBER = NumberFormat.getFormat("###");
+}
--- src/main/java/us/dontcareabout/CheckFlow/client/component/CheckListInfo.java
+++ src/main/java/us/dontcareabout/CheckFlow/client/component/CheckListInfo.java
@@ -2,7 +2,6 @@ package us.dontcareabout.CheckFlow.client.component;
 
 import java.util.Date;
 
-import com.google.gwt.i18n.client.NumberFormat;
 import com.sencha.gxt.chart.client.draw.Color;
 import com.sencha.gxt.chart.client.draw.RGB;
 import com.sencha.gxt.chart.client.draw.sprite.SpriteSelectionEvent;
@@ -13,16 +12,13 @@ import us.dontcareabout.CheckFlow.client.DateUtil;
 import us.dontcareabout.CheckFlow.client.Palette;
 import us.dontcareabout.CheckFlow.client.ui.UiCenter;
 import us.dontcareabout.CheckFlow.shared.CheckFlow;
-import us.dontcareabout.gxt.client.draw.LRectangleSprite;
 import us.dontcareabout.gxt.client.draw.LTextSprite;
 import us.dontcareabout.gxt.client.draw.LayerSprite;
 
 public class CheckListInfo extends LayerSprite {
 	public static final int HEIGHT = 140;
 
-	private static final NumberFormat NUMBER_FORMAT = NumberFormat.getFormat("###");
 	private static final int NAME_SIZE = 40;
-	private static final int NUMBER_SIZE = 60;
 	private static final int FOOTER_SIZE = 18;
 
 	private LTextSprite name = new LTextSprite();
@@ -110,46 +106,5 @@ public class CheckListInfo extends LayerSprite {
 
 		reciprocal.text.setFill(textColor);
 		reciprocal.footer.setFill(textColor);
-	}
-
-	private class Block extends LayerSprite {
-		LTextSprite text = new LTextSprite();
-		LTextSprite footer = new LTextSprite();
-		LRectangleSprite progress = new LRectangleSprite();
-
-		Block(String footerString) {
-			resize(NUMBER_SIZE * 1.5 + 30, HEIGHT - 10);
-			setLY(5);
-
-			progress.setWidth(getWidth());
-			progress.setFill(Palette.BLUE);
-			add(progress);
-
-			text.setTextBaseline(TextBaseline.MIDDLE);
-			text.setFontSize(NUMBER_SIZE);
-			text.setLY(50);
-			add(text);
-
-			footer.setText(footerString);
-			footer.setTextBaseline(TextBaseline.MIDDLE);
-			footer.setFontSize(FOOTER_SIZE);
-			footer.setLX(getWidth() - FOOTER_SIZE - 20);
-			footer.setLY(105);
-			add(footer);
-		}
-
-		void setNumber(double value) {
-			boolean isNaN = Double.isNaN(value);
-			text.setText(isNaN ? "？" : NUMBER_FORMAT.format(value));
-			double textWidth = isNaN ? NUMBER_SIZE :
-				text.getText().length() * NUMBER_SIZE / 2;
-			text.setLX(getWidth() - textWidth - 20);
-		}
-
-		void setProgress(double value) {
-			setNumber(value * 100);
-			progress.setHeight(getHeight() * value);
-			progress.setLY(getHeight() - progress.getHeight());
-		}
 	}
 }
