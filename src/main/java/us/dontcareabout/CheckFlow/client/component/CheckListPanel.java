fcf02ddf0bb6fc698d0bc126190b96d799bc7dcf
MontyPan <monty.pan@dontcareabout.us>
2018-07-14 19:09:04 +0800

    CheckPointLayer 降級成 CheckListPanel 的 inner class

--- src/main/java/us/dontcareabout/CheckFlow/client/component/CheckListPanel.java
+++ src/main/java/us/dontcareabout/CheckFlow/client/component/CheckListPanel.java
@@ -1,13 +1,16 @@
 package us.dontcareabout.CheckFlow.client.component;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+import com.google.common.base.Strings;
 import com.google.gwt.resources.client.ImageResource;
 import com.sencha.gxt.chart.client.draw.RGB;
 import com.sencha.gxt.chart.client.draw.sprite.SpriteSelectionEvent;
 import com.sencha.gxt.chart.client.draw.sprite.SpriteSelectionEvent.SpriteSelectionHandler;
 
+import us.dontcareabout.CheckFlow.client.DateUtil;
 import us.dontcareabout.CheckFlow.client.Palette;
 import us.dontcareabout.CheckFlow.client.Resources;
 import us.dontcareabout.CheckFlow.shared.CheckFlow;
@@ -92,6 +95,61 @@ public class CheckListPanel extends LayerContainer {
 		}
 
 		super.onResize(width, height);
+	}
+
+	class CheckPointLayer extends LayerSprite {
+		private CheckPoint data;
+		private LTextSprite text = new LTextSprite();
+		private LTextSprite reciprocal = new LTextSprite();
+
+		public CheckPointLayer() {
+			add(text);
+			add(reciprocal);
+		}
+
+		public void setData(CheckPoint checkPoint) {
+			this.data = checkPoint;
+			getText().setText(checkPoint.getName());
+
+			if (checkPoint.isFinish()) {
+				reciprocal.setText("");
+			} else {
+				if (checkPoint.getDeadline() == null) {
+					getReciprocal().setText("？");
+				} else {
+					int diff = DateUtil.daysBetween(checkPoint.getDeadline(), new Date());
+					String reciprocalHeader = diff < 0 ? "逾 " : "剩 ";
+					getReciprocal().setText(reciprocalHeader + Math.abs(diff) + " 天");
+				}
+			}
+
+			adjustMember();
+		}
+
+		@Override
+		protected void adjustMember() {
+			//沒有字就不用調了
+			if (Strings.isNullOrEmpty(getReciprocal().getText())) { return; }
+
+			getReciprocal().setLX(
+				getWidth() -
+				(getReciprocal().getText().length() + 2) *	//有兩個中文字補兩個 unit
+				getReciprocal().getFontSize() / 2
+				- 10	//右邊的 margin
+			);
+		}
+
+		public LTextSprite getText() {
+			return text;
+		}
+
+		public LTextSprite getReciprocal() {
+			return reciprocal;
+		}
+
+		public CheckPoint getData() {
+			return data;
+		}
 	}
 
 	class UnfinishLayer extends CheckPointLayer {
